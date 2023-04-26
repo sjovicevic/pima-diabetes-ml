84a8891f527753ae37a7be997a0a39b20c96f38b
Stefan Jovicevic <jovicevicst@gmail.com>
2023-04-26 03:54:47 +0200

    trained model with naive_bayes algorithm

--- src/pima-prediction.py
+++ src/pima-prediction.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.naive_bayes import GaussianNB
 import numpy as np
 
 df = pd.read_csv("../data/pima-data.csv")
@@ -24,6 +27,26 @@ print("Number of True cases: {0} ({1:2.2f}%)".format(num_true, percentage_of_num
 print("Number of False cases: {0} ({1:2.2f}%)".format(num_false, percentage_of_num_false))
 
 
+# 70% for training, 30% for testing
 
+feature_col_names = ['num_preg', 'glucose_conc', 'diastolic_bp', 'thickness', 'insulin', 'bmi', 'diab_pred', 'age']
+predicted_class_names = ['diabetes']
 
+X = df[feature_col_names].values
+y = df[predicted_class_names].values
+split_test_size = 0.30
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_test_size, random_state=42)
+
+print("{0:0.2f}% in training set".format((len(X_train)/len(df.index)) * 100))
+print("{0:0.2f}% in test set".format((len(X_test)/len(df.index)) * 100))
+
+fill_0 = SimpleImputer(missing_values = 0, strategy="mean")
+
+X_train = fill_0.fit_transform(X_train)
+X_test = fill_0.fit_transform(X_test)
+
+nb_model = GaussianNB()
+nb_model.fit(X_train, y_train.ravel())
+
+print(nb_model.get_params())
