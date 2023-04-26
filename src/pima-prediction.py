865e06f570f54f35348b134a1caf06e87100d3bd
Stefan Jovicevic <jovicevicst@gmail.com>
2023-04-26 03:14:33 +0200

    data ready for algorithm

--- src/pima-prediction.py
+++ src/pima-prediction.py
@@ -4,18 +4,26 @@ import numpy as np
 
 df = pd.read_csv("../data/pima-data.csv")
 
+# Deleted skin column because of redundant data
 del df['skin']
+
+# Changing diabetes values from True/False to 1/0 - for helping algorithm
 diabetes_map = {True : 1, False : 0}
 df['diabetes'] = df['diabetes'].map(diabetes_map)
 
+# Calculating the number of True/False cases
 num_true = len(df.loc[df['diabetes'] == True])
 num_false = len(df.loc[df['diabetes'] == False])
 
+# Calculating the percentage of True/False cases
 percentage_of_num_true = num_true / (num_true + num_false) * 100
 percentage_of_num_false = num_false / (num_true + num_false) * 100
 
+# Outputting the True/False cases
 print("Number of True cases: {0} ({1:2.2f}%)".format(num_true, percentage_of_num_true))
 print("Number of False cases: {0} ({1:2.2f}%)".format(num_false, percentage_of_num_false))
+
+
 
 
 
